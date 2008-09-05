a9201d6b7014f768e9328ba499f0840f05fdd4b9
Luke Wilson-Mawer <lukewilsonmawer@luke-wilson-mawers-macbook.local>
2008-09-05 15:58:23 +0100

    Some more session stuff.  Writes to files in /tmp/foafeditor

---
+++ application/models/FoafSessionHandler.php
@@ -0,0 +1,40 @@
+<?php
+class FoafSessionHandler implements Zend_Session_SaveHandler_Interface{
+	
+	function close(){
+		return true;
+	}
+	function open($save_path,$name){
+		return true;
+		
+	}
+	function write($id,$data){
+		$dir_path = "/tmp/foafeditor_sessions";
+		if( !is_dir($dir_path)){
+			 mkdir( $dirPath );
+		}
+ 	 	$session_file = $dir_path."/session_".$id;
+ 	 	$handle = @fopen($session_file, "w+");
+  		if ($handle) {
+  			
+    		$return = fwrite($handle, $data);
+    		fclose($handle);
+    		return $return;
+  		} else {
+    		return false;
+  		}	
+	}
+	function read($id){
+ 	 	$session_file = "/tmp/foafeditor_sessions/session_$id";
+  		return (string)@file_get_contents($session_file);	
+	}
+	function destroy($id){
+  		$session_file = "/tmp/foafeditor_sessions/session_$id";
+  		return unlink($session_file);
+	}
+	function gc($maxlifetime){
+	
+	}
+
+}
+?>
