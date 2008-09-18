d48d48c48d8cd97a9fd35a2b21fb1079f9588567
Luke Wilson-Mawer <lukewilsonmawer@luke-wilson-mawers-macbook.local>
2008-09-18 16:46:05 +0100

    Added new views and a half done accounts implementation.

--- application/models/FieldNames.php
+++ application/models/FieldNames.php
@@ -35,10 +35,30 @@ class FieldNames {
 			case "pictures":
 				$this->instantiatePicturesFields();
 				break;
+			case "accounts":
+				$this->instantiateAccountsFields();
+				break;
+			case "friends":
+				$this->instantiateFriendsFields();
+				break;
+			case "blogs":
+				$this->instantiateBlogsFields();
+				break;
+			case "interests":
+				$this->instantiateInterestsFields();
+				break;
+			case "other":
+				$this->instantiateOtherFields();
+				break;
 			case "all":
 				$this->instantiateTheBasicsFields();
 				$this->instantiateContactDetailsFields();
 				$this->instantiatePicturesFields();
+				$this->instantiateAccountsFields();
+				$this->instantiateFriendsFields();
+				$this->instantiateBlogsFields();
+				$this->instantiateInterestsFields();
+				$this->instantiateOtherFields();
 				break;	
 		}
 	}
@@ -46,21 +66,21 @@ class FieldNames {
 	/*instantiates arrays of fields for all the items on the basics page*/
 	private function instantiateTheBasicsFields(){
 		$this->allFieldNames['foafTitle'] = 
-			new SimpleField('foafTitle', 'Title', '?x foaf:title ?foafTitle', 'literal',"http://xmlns.com/foaf/0.1/title");
+			new SimpleField('foafTitle', 'Title', '?x foaf:title ?foafTitle', 'literal',false, "http://xmlns.com/foaf/0.1/title");
 		$this->allFieldNames['foafGivenName'] = 
-			new SimpleField('foafGivenName', 'Given Name', '?x foaf:givenname ?foafGivenName', 'literal',"http://xmlns.com/foaf/0.1/givenname");
+			new SimpleField('foafGivenName', 'Given Name', '?x foaf:givenname ?foafGivenName', 'literal',false,"http://xmlns.com/foaf/0.1/givenname");
 		$this->allFieldNames['foafFamilyName'] = 
-			new SimpleField('foafFamilyName', 'Family Name', '?x foaf:family_name ?foafFamilyName', 'literal',"http://xmlns.com/foaf/0.1/family_name");
+			new SimpleField('foafFamilyName', 'Family Name', '?x foaf:family_name ?foafFamilyName', 'literal',false, "http://xmlns.com/foaf/0.1/family_name");
 		$this->allFieldNames['foafName'] = 
-			new SimpleField('foafName', 'Real Name', '?x foaf:name ?foafName', 'literal','http://xmlns.com/foaf/0.1/name');
+			new SimpleField('foafName', 'Real Name', '?x foaf:name ?foafName', 'literal',false,'http://xmlns.com/foaf/0.1/name');
 		$this->allFieldNames['foafHomepage'] = 
-			new SimpleField('foafHomepage', 'Homepages', '?x foaf:homepage ?foafHomepage','resource','http://xmlns.com/foaf/0.1/homepage');
+			new SimpleField('foafHomepage', 'Homepages', '?x foaf:homepage ?foafHomepage','resource',false,'http://xmlns.com/foaf/0.1/homepage');
 		$this->allFieldNames['foafNick'] = 
-			new SimpleField('foafNick', 'Nicknames', '?x foaf:nick ?foafNick','literal','http://xmlns.com/foaf/0.1/nick');
+			new SimpleField('foafNick', 'Nicknames', '?x foaf:nick ?foafNick','literal',false,'http://xmlns.com/foaf/0.1/nick');
 		$this->allFieldNames['foafDateOfBirth'] = 
-			new SimpleField('foafDateOfBirth', 'Birthday', '?x foaf:dateOfBirth ?foafDateOfBirth','literal','http://xmlns.com/foaf/0.1/dateOfBirth');
+			new SimpleField('foafDateOfBirth', 'Birthday', '?x foaf:dateOfBirth ?foafDateOfBirth','literal',false,'http://xmlns.com/foaf/0.1/dateOfBirth');
 		$this->allFieldNames['foafBirthday'] = 
-			new SimpleField('foafBirthday', 'Birthday', '?x foaf:birthday ?foafBirthday','literal','http://xmlns.com/foaf/0.1/birthday');
+			new SimpleField('foafBirthday', 'Birthday', '?x foaf:birthday ?foafBirthday','literal',false,'http://xmlns.com/foaf/0.1/birthday');
 					
 		$this->allFieldNames['bioBirthday'] = new BioBirthdayField();
 		$this->allFieldNames['geoLatLong'] =  new GeoLatLongField();
@@ -70,17 +90,55 @@ class FieldNames {
 	
 	private function instantiateContactDetailsFields(){
 		$this->allFieldNames['foafMbox'] = 
-			new SimpleField('foafMbox', 'Email', '?x foaf:mbox ?foafMbox', 'literal',"http://xmlns.com/foaf/0.1/mbox");
+			new SimpleField('foafMbox', 'Email', '?x foaf:mbox ?foafMbox', 'literal',false,"http://xmlns.com/foaf/0.1/mbox");
 		$this->allFieldNames['foafPhone'] = 
-			new SimpleField('foafPhone', 'Phone', '?x foaf:phone ?foafPhone', 'literal','http://xmlns.com/foaf/0.1/phone');
+			new SimpleField('foafPhone', 'Phone', '?x foaf:phone ?foafPhone', 'literal',false,'http://xmlns.com/foaf/0.1/phone');
 		$this->allFieldNames['foafMbox_sha1sum'] = 
-			new SimpleField('foafMbox_sha1sum', 'Email (sha1sum)', '?x foaf:mbox_sha1sum ?foafMbox_sha1sum', 'literal','http://xmlns.com/foaf/0.1/mbox_sha1sum');
-	
+			new SimpleField('foafMbox_sha1sum', 'Email (sha1sum)', '?x foaf:mbox_sha1sum ?foafMbox_sha1sum', 'literal',false,'http://xmlns.com/foaf/0.1/mbox_sha1sum');
+		
 	}
 	
 	private function instantiatePicturesFields(){
 		$this->allFieldNames['foafDepiction'] = 
-			new SimpleField('foafDepiction','Image', '?x foaf:depiction ?foafDepiction', 'resource',"http://xmlns.com/foaf/0.1/depiction");
+			new SimpleField('foafDepiction','Image', '?x foaf:depiction ?foafDepiction', 'resource',false,"http://xmlns.com/foaf/0.1/depiction");
+	}
+	
+	private function instantiateAccountsFields(){
+		$this->allFieldNames['accountServiceHomepage'] = 
+			new SimpleField('accountServiceHomepage', 'Account Service Homepage', '
+				?x foaf:holdsAccount ?a . 
+				?a rdf:type foaf:OnlineAccount 
+				OPTIONAL{
+					?a foaf:accountProfilePage ?accountProfilePage .
+				} .
+				OPTIONAL{
+					?a foaf:accountServiceHomepage ?accountServiceHomepage .
+				} .
+				OPTIONAL{
+					?a foaf:accountName ?accountName .
+				}'
+			, 'resource',true,'http://xmlns.com/foaf/0.1/accountServiceHomepage');
+		/*note: no queries for this as everything is included in the query above*/
+		$this->allFieldNames['accountProfilePage'] = 
+			new SimpleField('accountProfilePage', 'Account Profile Homepage', '', 'resource',true, 'http://xmlns.com/foaf/0.1/accountProfilePage');
+		$this->allFieldNames['accountName'] = 
+			new SimpleField('accountName', 'Account Name', '', 'resource',true, 'http://xmlns.com/foaf/0.1/accountName');
+	}
+	
+	private function instantiateFriendsFields(){
+		
+	}
+	
+	private function instantiateBlogsFields(){
+		
+	}
+	
+	private function instantiateInterestsFields(){
+		
+	}
+	
+	private function instantiateOtherFields(){
+		
 	}
 	
 	public function getAllFieldNames(){
