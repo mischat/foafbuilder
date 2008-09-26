f4d1be57f5617e11ef9ec8886673d9e16e035248
Mischa Tuffield <mischatuffield@float.local>
2008-09-26 10:15:41 +0100

    Made changes to the html, and made first change to the loadFoaf functionality

--- application/models/FoafSessionHandler.php
+++ application/models/FoafSessionHandler.php
@@ -1,54 +1,59 @@
 <?php
-class FoafSessionHandler implements Zend_Session_SaveHandler_Interface{
+
+class FoafSessionHandler implements Zend_Session_SaveHandler_Interface {
 	
-	function close(){
-		return true;
-	}
-	function open($save_path,$name){
-		return true;
-		
-	}
-	function write($id,$data){
-		
-		$dir_path = "/tmp/foafeditor_sessions";
-		if( !is_dir($dir_path)){
-			 mkdir( $dirPath );
-		}
- 	 	$session_file = $dir_path."/session_".$id;
- 	 	$handle = @fopen($session_file, "w");
-  		if ($handle) {
-    		$return = fwrite($handle, $data);
-    		fclose($handle);
-    		return $return;
-  		} else {
-    		return false;
-  	    }			
-	}
-	function read($id){
-		$session_file = "/tmp/foafeditor_sessions/session_$id";
-  		return (string)@file_get_contents($session_file);	
-	}
-	function destroy($id){
-  		$session_file = "/tmp/foafeditor_sessions/session_$id";
-  		return unlink($session_file);
-	}
-	function gc($maxlifetime){
-		//a test run
-		$dir_path = "/tmp/foafeditor_sessions";
-		if( !is_dir($dir_path)){
-			 mkdir( $dirPath );
-		}
- 	 	$session_file = $dir_path."/gc_test_".$id;
- 	 	$handle = @fopen($session_file, "w");
-  		if ($handle) {
-  			
-    		$return = fwrite($handle, $data);
-    		fclose($handle);
-    		return $return;
-  		} else {
-    		return false;
-  		}
-	}
+    function close() {
+        return true;
+    }
+    function open($save_path,$name) {
+        return true;
+    }
+
+    //TODO This needs to do some error catching, no dir, no write permission etc
+    function write($id,$data)   {
+        $dir_path = "/tmp/foafeditor_sessions";
+        if( !is_dir($dir_path)){
+            mkdir( $dirPath );
+        }
+        $session_file = $dir_path."/session_".$id;
+        $handle = @fopen($session_file, "w");
+        if ($handle) {
+            $return = fwrite($handle, $data);
+            fclose($handle);
+            return $return;
+        } else {
+            return 0;
+        }			
+    }
+
+    function read($id) {
+        $session_file = "/tmp/foafeditor_sessions/session_$id";
+        return (string)@file_get_contents($session_file);	
+    }
+
+    function destroy($id) {
+        $session_file = "/tmp/foafeditor_sessions/session_$id";
+        return (unlink($session_file));
+    }
+
+    //TODO rename function
+    function gc($maxlifetime) {
+        //a test run
+        $dir_path = "/tmp/foafeditor_sessions";
+        if( !is_dir($dir_path)){
+            mkdir( $dirPath );
+        }
+
+        $session_file = $dir_path."/gc_test_".$id;
+        $handle = @fopen($session_file, "w");
+        if ($handle) {
+            $return = fwrite($handle, $data);
+            fclose($handle);
+            return $return;
+        } else {
+            return false;
+        }
+    }
 
 }
-?>
+/* vi:set expandtab sts=4 sw=4: */
