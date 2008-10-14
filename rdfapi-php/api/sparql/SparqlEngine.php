7865e3800c393e34f54e01a59d3b3f39b340cb7e
Luke Wilson-Mawer <lukewilsonmawer@luke-wilson-mawers-macbook.local>
2008-10-14 10:24:48 +0100

    Fix to rap code to ensure correct joining of optionals.

--- rdfapi-php/api/sparql/SparqlEngine.php
+++ rdfapi-php/api/sparql/SparqlEngine.php
@@ -408,12 +408,13 @@ Class SparqlEngine extends Object{
 
         if(!$finalRes && !$res)
         return array();
-
-        if(!$finalRes)
-        return $res;
-
-        if(!$res)
-        return $finalRes;
+	
+        if(!$finalRes){
+        	return $res;
+	}
+        if(!$res){
+        	return array();
+	}
 
         // find joint variables and new variables to be added to $finalRes
         $jointVars = array();
@@ -593,34 +594,23 @@ Class SparqlEngine extends Object{
     * @return Array
     */
     protected function selectVars($table,$vars){
-        //XXX
-        if($vars[0]->variable=='*') {
-                $vars = $this->query->getAllVars();
-        }
+        if($vars[0]=='*')
+        $vars = $this->query->getAllVars();
         $resTable = array();
         $hits = 0;
-        foreach($table as $val) {
-             foreach($vars as $var) {
-		//PHP 5.1.6
-		if(!is_array($var)) {
-			if(isset($val[$var->variable])) {
-			    $resTable[$hits][$var->variable]=$val[$var->variable];
-			} else {
-			    $resTable[$hits][$var->variable]="";
-			}
-		//PHP 5.2.2
-		} else {
-			if(isset($val[(string) $var])){
-			    $resTable[$hits][(string) $var]=$val[(string) $var];
-			}else{
-			    $resTable[$hits][(string) $var]="";
-			}
-		}
+        foreach($table as $val){
+            foreach($vars as $var){
+                if(isset($val[(string)$var])){
+                    $resTable[$hits][(string)$var]=$val[(string)$var];
+                }else{
+                    $resTable[$hits][(string)$var]="";
+                }
             }
             $hits++;
         }
         return $resTable;
     }
+
     /**
     * Joins the results of the different Graphpatterns.
     *
