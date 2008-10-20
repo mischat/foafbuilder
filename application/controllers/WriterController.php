ac3cfa15eeef666bc582c4bf551474cde2748c8a
Mischa Tuffield <mischatuffield@float.local>
2008-10-20 13:39:32 +0100

    Made changes to RAP, to support writing n3,nt to HTML and to String Added support for the write-foafn3

--- application/controllers/WriterController.php
+++ application/controllers/WriterController.php
@@ -43,5 +43,42 @@ class WriterController extends Zend_Controller_Action
         }
     }
 
+    /* This is the writeFoafN3 */
+    public function writeFoafn3Action() {
+        require_once 'FoafData.php';
+
+        $foafData = FoafData::getFromSession();
+    
+        if ($foafData) {
+            $this->view->model = $foafData->getModel();
+            $this->view->uri = $foafData->getURI();
+            $this->view->graphset= $foafData->getGraphset();
+            $this->view->primaryTopic = $foafData->getPrimaryTopic();
+
+            $newDocUri = @$_POST['uri'];
+            if (!$newDocUri) {
+                $newDocUri = $this->view->uri;
+            }
+
+            $newDocUriRes = new Resource($newDocUri);
+            $newPersonUriRes = new Resource($newDocUri."#me");
+            $oldPersonUriRes = new Resource($this->view->primaryTopic);
+            $oldDocUriRes = new Resource($this->view->uri);
+
+            $this->view->model->replace($oldDocUriRes,new Resource("<http://xmlns.com/foaf/0.1/primaryTopic>"),NULL,$newDocUriRes);
+            $this->view->model->replace($oldPersonUriRes,NULL,NULL,$newPersonUriRes);
+            $this->view->model->replace(NULL,NULL,$oldPersonUriRes,$newPersonUriRes);
+
+            $this->view->model->setBaseUri(NULL);
+            $result = $this->view->model->find(NULL, NULL, NULL);
+            //echo($result->writeRdfToString('n3'));
+            echo($result->writeAsHTML('n3'));
+            //FIXME: change this back
+            //echo($result->writeAsHtmlTable());
+        } else {
+            echo("Nothing to Write, Session is empty");
+        }
+    }
+
 }
 /* vi:set expandtab sts=4 sw=4: */
