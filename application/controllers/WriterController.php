0b28c36bea6c205b21e1a64ae2cd4908492310f4
Luke Wilson-Mawer <lukewilsonmawer@luke-wilson-mawers-macbook.local>
2008-11-24 17:38:09 +0000

    2 Text fields for geek view but no saving for it.

--- application/controllers/WriterController.php
+++ application/controllers/WriterController.php
@@ -8,77 +8,94 @@ class WriterController extends Zend_Controller_Action
         $this->view->baseUrl = $this->_request->getBaseUrl();
     }
 
-    public function writeFoafAction() {
-        require_once 'FoafData.php';
-
-        $foafData = FoafData::getFromSession();
-    
-        if ($foafData) {
-            $this->view->model = $foafData->getModel();
-            $this->view->uri = $foafData->getURI();
-            $this->view->graphset= $foafData->getGraphset();
-            $this->view->primaryTopic = $foafData->getPrimaryTopic();
-
-            $newDocUri = @$_POST['uri'];
-            if (!$newDocUri) {
-                $newDocUri = $this->view->uri;
-            }
-
-            $newDocUriRes = new Resource($newDocUri);
-            $newPersonUriRes = new Resource($newDocUri."#me");
-            $oldPersonUriRes = new Resource($this->view->primaryTopic);
-            $oldDocUriRes = new Resource($this->view->uri);
-
-            $this->view->model->replace($oldDocUriRes,new Resource("<http://xmlns.com/foaf/0.1/primaryTopic>"),NULL,$newDocUriRes);
-            $this->view->model->replace($oldPersonUriRes,NULL,NULL,$newPersonUriRes);
-            $this->view->model->replace(NULL,NULL,$oldPersonUriRes,$newPersonUriRes);
-
-            $this->view->model->setBaseUri(NULL);
-            $result = $this->view->model->find(NULL, NULL, NULL);
-            echo($result->writeRdfToString());
-            //FIXME: change this back
-            //echo($result->writeAsHtmlTable());
-        } else {
-            echo("Nothing to Write, Session is empty");
-        }
-    }
-
-    /* This is the writeFoafN3 */
     public function writeFoafn3Action() {
         require_once 'FoafData.php';
-
-        $foafData = FoafData::getFromSession();
+        
+        /*get the private and public stuff from the session*/
+        $privateFoafData = FoafData::getFromSession(false);
+    	$publicFoafData = FoafData::getFromSession(true);
+    	
+    	/*if there is a uri then we need to use that*/
+        $newDocUri = @$_POST['uri'];
+        
+        /*where the data will be stored in the view*/
+        $this->view->data = new Object();
+        
+        if($privateFoafData) {
+        	$this->doWrite($privateFoafData,$newDocUri,true);
+        }
+        if($publicFoafData){
+        	$this->doWrite($publicFoafData,$newDocUri,true);
+        }
+    }
     
-        if ($foafData) {
-            $this->view->model = $foafData->getModel();
+  public function writeFoafAction() {
+        require_once 'FoafData.php';
+        
+        /*get the private and public stuff from the session*/
+        $privateFoafData = FoafData::getFromSession(false);
+    	$publicFoafData = FoafData::getFromSession(true);
+    	
+    	/*if there is a uri then we need to use that*/
+        $newDocUri = @$_POST['uri'];
+        
+        /*where the data will be stored in the view*/
+        $this->view->data = new Object();
+        
+        if($privateFoafData) {
+        	$this->doWrite($privateFoafData,$newDocUri);
+        }
+        if($publicFoafData){
+        	$this->doWrite($publicFoafData,$newDocUri);
+        }
+    }
+    
+    private function doWrite($foafData,$newDocUri,$writeNtriples){
+  
+    		if(!$foafData){
+    			return;
+    		}
+
+    		/*put some stuff in the view*/
+    		//XXX is this really necessary
+    	    $this->view->model = $foafData->getModel();
             $this->view->uri = $foafData->getURI();
             $this->view->graphset= $foafData->getGraphset();
             $this->view->primaryTopic = $foafData->getPrimaryTopic();
 
-            $newDocUri = @$_POST['uri'];
             if (!$newDocUri) {
                 $newDocUri = $this->view->uri;
             }
-
+			
+            /*do some mangling with the primary topic to set it to the passed uri*/
             $newDocUriRes = new Resource($newDocUri);
             $newPersonUriRes = new Resource($newDocUri."#me");
             $oldPersonUriRes = new Resource($this->view->primaryTopic);
             $oldDocUriRes = new Resource($this->view->uri);
-
+            
             $this->view->model->replace($oldDocUriRes,new Resource("<http://xmlns.com/foaf/0.1/primaryTopic>"),NULL,$newDocUriRes);
             $this->view->model->replace($oldPersonUriRes,NULL,NULL,$newPersonUriRes);
             $this->view->model->replace(NULL,NULL,$oldPersonUriRes,$newPersonUriRes);
-
             $this->view->model->setBaseUri(NULL);
+            
+            /*get everything and put it in the view*/
             $result = $this->view->model->find(NULL, NULL, NULL);
-            echo($result->writeRdfToString('nt'));
-            //echo($result->writeAsHTML('n3'));
-            //FIXME: change this back
-            //echo($result->writeAsHtmlTable());
-        } else {
-            echo("Nothing to Write, Session is empty");
-        }
+            
+            if($foafData->isPublic){
+            	if($writeNtriples){
+            		$this->view->data->public = $result->writeRdfToString('nt');
+            	} else {
+            		$this->view->data->public = $result->writeRdfToString();
+            	}
+            } else {
+            	if($writeNtriples){
+            		$this->view->data->private = $result->writeRdfToString('nt');
+            	} else {
+            		$this->view->data->private = $result->writeRdfToString();
+            	}	
+            }    	
     }
 
+
+
 }
-/* vi:set expandtab sts=4 sw=4: */
