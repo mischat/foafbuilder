3f3973dd5b46f3319b4c900643a2072cafbfbb1a
Mischa Tuffield <mischa.tuffield@garlik.com>
2008-12-01 12:37:49 +0000

    Fixed the OpenID login, now it reports in the error_log

--- application/controllers/LogonController.php
+++ application/controllers/LogonController.php
@@ -23,19 +23,20 @@ class LogonController extends Zend_Controller_Action
 		$consumer = new Zend_OpenId_Consumer();
 
 		var_dump($consumer);
-		if (!$consumer->login($_POST['openid_identifier'])) {
-			error_log("OpenID login failed.<br>");
-		} else {
-			error_log("Does this make it valid?");
-		}
+		//if (!$consumer->login($_POST['openid_identifier'])) {
+		if (!$consumer->login($openid)) {
+			error_log("OpenID login failed.");
+		} 
 	} else if (isset($_GET['openid_mode'])) {
-    		error_log("Please say something is happening here".$_GET['openid_mode']);
+    		error_log("Please say something is happening here: ".$_GET['openid_mode']);
 	       if ($_GET['openid_mode'] == "id_res") {
 			$consumer = new Zend_OpenId_Consumer();
 			if ($consumer->verify($_GET, $id)) {
 			    error_log("VALID " . htmlspecialchars($id));
+				//ADD Authenicated to the Session!
 			} else {
 			    error_log("INVALID " . htmlspecialchars($id));
+				//REMOVE from session
 			}
 		} else if ($_GET['openid_mode'] == "cancel") {
 			error_log("CANCELED");
