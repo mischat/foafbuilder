671ef3a6853de95caab82fc331c3789e14e4999b
Luke Wilson-Mawer <lukewilsonmawer@luke-wilson-mawers-macbook.local>
2008-12-01 14:54:09 +0000

    New accounts controller.

--- application/controllers/AjaxController.php
+++ application/controllers/AjaxController.php
@@ -81,56 +81,14 @@ class AjaxController extends Zend_Controller_Action {
 				$this->view->results['lastfmFound'] = true;
 				$this->foafData->lastfmFound = true;
 			}
-        } 
-        
+        }   
         
         //$result = $this->foafData->getModel()->find(NULL, NULL, NULL);
         //echo($result->writeRdfToString('nt'));
 	}
 
 
-	//converts usernames into uris
-	public function usernameToUriAction(){
-		
-		//build a query from the names of the post variables to get the appropriate patterns
-		$query = "PREFIX gs: <http://qdos.com/schema#> 
-                        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
-                        SELECT ?serv ?patt WHERE 
-                        { ?serv rdfs:subPropertyOf gs:serviceProperty ; gs:canonicalUriPattern ?patt .
-                        FILTER(";
-        //we only want to get canonical patterns of things that are passed in
-		foreach($_POST as $key => $value){
-			$query.='?serv = <http://qdos.com/schema#'.$key.'> || ';			
-		}
-		$query = substr($query,0,-3).')}';
-		
-		//execute the query to get the patterns
-		$res = sparql_query(QDOS_EP,$query);
-		echo($query);
-		var_dump($res);
-                if(!$res || empty($res)){
-                        return;
-                }
-
-		//use this to contain the patterns for all of the usernames passed in
-                $uris = array();
-          
-                foreach($res as $row){
-                        if(!isset($row['?serv']) || !$row['?serv']){
-                                continue;
-                        } 
-                        if(!isset($row['?patt']) || !$row['?patt']){
-                                continue;
-                        }
-                        //create the various uris
-			foreach($_POST as $key => $value){
-                        	if(sparql_strip($row['?serv']) == 'http://qdos.com/schema#'.$key){
-					$uris[$key] = str_replace('@USER@',$value,sparql_strip($row['?patt']));
-				}
-			}
-		}
-                $this->view->results = $uris;
-	}
+	
     public function loadTheBasicsAction() {
     	$this->loadAnyPage('theBasics');
     }
