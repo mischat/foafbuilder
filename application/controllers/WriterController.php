d9c19bc6200c135f4d6da1387e4b4743a410d768
Mischa Tuffield <mischa.tuffield@garlik.com>
2008-12-01 14:40:13 +0000

    Added a "cleanup" language tags option to the WriterController

--- application/controllers/WriterController.php
+++ application/controllers/WriterController.php
@@ -29,6 +29,38 @@ class WriterController extends Zend_Controller_Action
         }
     }
 
+  /* This should remove all unnessary Language Tags from the outputted RDF */
+  public function removeLanguageTags($results) {
+
+	foreach ($results->triples as $result) {
+		if ($result->pred->uri == "http://xmlns.com/foaf/0.1/jabberID") {
+			$result->obj->lang = NULL;
+		} else if ($result->pred->uri == "http://xmlns.com/foaf/0.1/aimChatID") {
+			$result->obj->lang = NULL;
+		} else if ($result->pred->uri == "http://xmlns.com/foaf/0.1/msnChatID") {
+			$result->obj->lang = NULL;
+		} else if ($result->pred->uri == "http://xmlns.com/foaf/0.1/mbox_sha1sum") {
+			$result->obj->lang = NULL;
+		} else if ($result->pred->uri == "http://purl.org/dc/elements/1.1/format") {
+			$result->obj->lang = NULL;
+		} else if ($result->pred->uri == "http://xmlns.com/foaf/0.1/myersBriggs") {
+			$result->obj->lang = NULL;
+		} else if ($result->pred->uri == "http://xmlns.com/foaf/0.1/accountName") {
+			$result->obj->lang = NULL;
+		} else if ($result->pred->uri == "http://xmlns.com/wot/0.1/keyid") {
+			$result->obj->lang = NULL;
+		} else if ($result->pred->uri == "http://purl.org/net/inkel/rdf/schemas/lang/1.1#masters") {
+			$result->obj->lang = NULL;
+		} else if ($result->pred->uri == "http://www.megginson.com/exp/ns/airports#icao") {
+			$result->obj->lang = NULL;
+		} else if ($result->pred->uri == "http://www.megginson.com/exp/ns/airports#iata") {
+			$result->obj->lang = NULL;
+		}	
+	}
+	return $results;
+  }
+
+  /* This function should be used to download a FOAF file to your desktop */
   public function writeFoafn3PublicAction() {
         require_once 'FoafData.php';
 	//this is inside an action in one of your controllers:
@@ -37,6 +69,9 @@ class WriterController extends Zend_Controller_Action
 	$this->view->model = $publicFoafData->getModel();
 	$this->view->model->setBaseUri(NULL);
 	$result = $this->view->model->find(NULL, NULL, NULL);
+
+	$result = $this->removeLanguageTags($result);
+
 	$data = $result->writeRdfToString();
 	$hash = $publicFoafData->getURI();
 
@@ -82,35 +117,10 @@ class WriterController extends Zend_Controller_Action
     
     private function doWrite($foafData,$newDocUri,$writeNtriples){
   
-    		if(!$foafData){
-    			return;
-    		}
+	    if (!$foafData) {
+		return;
+	    }
 
-	/*
-    	    $this->view->model = $foafData->getModel();
-            $this->view->uri = $foafData->getURI();
-            $this->view->graphset= $foafData->getGraphset();
-            $this->view->primaryTopic = $foafData->getPrimaryTopic();
-
-            if (!$newDocUri) {
-                $newDocUri = $this->view->uri;
-            }
-			
-            //do some mangling with the primary topic to set it to the passed uri
-            $newDocUriRes = new Resource($newDocUri);
-            $newPersonUriRes = new Resource($newDocUri."#me");
-            $oldPersonUriRes = new Resource($this->view->primaryTopic);
-            $oldDocUriRes = new Resource($this->view->uri);
-            
-            $this->view->model->replace($oldDocUriRes,new Resource("<http://xmlns.com/foaf/0.1/primaryTopic>"),NULL,$newDocUriRes);
-            $this->view->model->replace($oldPersonUriRes,NULL,NULL,$newPersonUriRes);
-            $this->view->model->replace(NULL,NULL,$oldPersonUriRes,$newPersonUriRes);
-            $this->view->model->setBaseUri(NULL);
-            
-            /*get everything and put it in the view
-            $result = $this->view->model->find(NULL, NULL, NULL);
-
-	*/
 	    //TODO MISCHA ... use the __clone() Magic Method instead, this is not optimum
 	    $tempmodel = unserialize(serialize($foafData->getModel()));
             $tempuri = $foafData->getURI();
