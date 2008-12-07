428c41546a1f64e44f8a928f6251f75e6368420e
Mischa Tuffield <mischa.tuffield@garlik.com>
2008-12-07 23:23:52 +0000

    Remove OWL:SAMEAS old url, it causes too many problems, with their owl:sameAs resolution!

--- application/models/FoafData.php
+++ application/models/FoafData.php
@@ -127,7 +127,8 @@ class FoafData {
 	        
 	        //XXX speak to mischa about this one
 	        if (!preg_match("/#me$/",$oldPrimaryTopic,$patterns)) {
-	        	 $this->model->add(new Statement($newPrimaryTopicRes,new Resource("http://www.w3.org/2002/07/owl#sameAs"),$oldPrimaryTopicRes));
+//TODO MISCHA  I had to remove these, perhaps we should put in RDFs:SeeAlso's
+//	        	 $this->model->add(new Statement($newPrimaryTopicRes,new Resource("http://www.w3.org/2002/07/owl#sameAs"),$oldPrimaryTopicRes));
 	        }
         } 
         
@@ -187,7 +188,8 @@ error_log("replacing $oldPrimaryTopic with $newPrimaryTopic");
 	        
 	        //XXX speak to mischa about this one
 	        if (!preg_match("/#me$/",$oldPrimaryTopic,$patterns)) {
-	        	 $this->model->add(new Statement($newPrimaryTopicRes,new Resource("http://www.w3.org/2002/07/owl#sameAs"),$oldPrimaryTopicRes));
+//TODO MISCHA  I had to remove these, perhaps we should put in RDFs:SeeAlso's
+//	        	 $this->model->add(new Statement($newPrimaryTopicRes,new Resource("http://www.w3.org/2002/07/owl#sameAs"),$oldPrimaryTopicRes));
 	        }
         } 
         
