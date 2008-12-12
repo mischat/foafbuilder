e8bf1b783865d2d00addb25c616eabb097e1b976
Luke Wilson-Mawer <luke.wilson-mawer@garlik.com>
2008-12-12 16:44:22 +0000

    New qdos root and path settings to point to dev instance.

--- application/models/helpers/settings.php
+++ application/models/helpers/settings.php
@@ -1,11 +1,11 @@
 <?php
 
-define('FOAF_ROOT', 'http://foaf.qdos.com/');
+define('FOAF_ROOT', 'http://luke.foaf.qdos.com/');
 define('FOAF_EP', 'http://luma:8083/sparql/');
 define('FOAF_ALT_EP', 'http://luma:8082/sparql/');
 define('QDOS_EP', 'http://luma:8080/sparql/');
 define('QDOS_EXT', 'http://extractor.qdos.com:8081/');
-define('FOAF_PATH', '/usr/local/src/qdos-dev/foaf');
+define('FOAF_PATH', '/usr/local/src/qdos-luke/foaf');
 define('QDOS_ROOT', 'http://qdos.com/');
 
 //TODO MISCHA
