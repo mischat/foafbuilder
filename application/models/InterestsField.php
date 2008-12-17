4c7173b641cf1854e0b8df7113f7c136246cd5dc
Mischa Tuffield <mischa.tuffield@garlik.com>
2008-12-17 17:52:39 +0000

    Fixed the duplication in the interests field

--- application/models/InterestsField.php
+++ application/models/InterestsField.php
@@ -64,6 +64,7 @@ class InterestsField extends Field {
 		$privacy = 'private';			
 	    }
 
+
 	    /*mangle the results so that they can be easily rendered*/
 	    foreach ($results as $row) {	
 	    	$interestsArray = array();
@@ -103,8 +104,9 @@ class InterestsField extends Field {
 	
 	$dctitle_res = new Resource("http://purl.org/dc/elements/1.1/title");
 	foreach($value->values as $thisValue){
-		$foafData->getModel()->add(new Statement ($primary_topic_resource,$interest_resource,new Resource((string) $thisValue->uri))); 
-		$foafData->getModel()->add(new Statement (new Resource((string)$thisValue->uri),$dctitle_res,new Literal((string) $thisValue->title))); 
+error_log("Am writng a new triple");
+		$foafData->getModel()->addWithoutDuplicates(new Statement ($primary_topic_resource,$interest_resource,new Resource((string) $thisValue->uri))); 
+		$foafData->getModel()->addWithoutDuplicates(new Statement (new Resource((string)$thisValue->uri),$dctitle_res,new Literal((string) $thisValue->title))); 
 	}
     }
 }
