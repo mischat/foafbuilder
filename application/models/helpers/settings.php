55967c4f4a3ae9bac0381b378f0f555d69029861
= <=>
2008-12-18 17:00:12 +0000

    More settings stuff for Live version

--- application/models/helpers/settings.php
+++ application/models/helpers/settings.php
@@ -5,7 +5,7 @@ define('FOAF_EP', 'http://luma:8083/sparql/');
 define('FOAF_ALT_EP', 'http://luma:8082/sparql/');
 define('QDOS_EP', 'http://luma:8080/sparql/');
 define('QDOS_EXT', 'http://extractor.qdos.com:8081/');
-define('FOAF_PATH', '/usr/local/src/qdos-luke/foaf');
+define('FOAF_PATH', '/usr/local/src/qdos-dev/foaf');
 define('QDOS_ROOT', 'http://qdos.com/');
 
 //TODO MISCHA
@@ -20,6 +20,4 @@ define('BUILDER_TEMP_DIR','/tmp/foafbuilder_temporary_file');
 
 define('PRIVATE_EP','http://luma:8090/sparql/');
 define('PUBLIC_EP', 'http://luma:8083/sparql/');
-
-
 ?>
