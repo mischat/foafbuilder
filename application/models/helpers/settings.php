25395d513812c60e978d1cbc48989360cff7a707
Mischa Tuffield <mischa.tuffield@garlik.com>
2009-01-05 17:19:05 +0000

    Fixed OpenID login weirdness, changed login text on splash screen

--- application/models/helpers/settings.php
+++ application/models/helpers/settings.php
@@ -9,7 +9,6 @@ define('FOAF_PATH', '/usr/local/src/qdos-dev/foaf');
 define('QDOS_ROOT', 'http://qdos.com/');
 
 /*Live Settings*/
-/*
 define('BUILDER_URL','http://foafbuilder.qdos.com/');
 define('IMAGE_URL',BUILDER_URL.'images/');
 define('PUBLIC_URL',BUILDER_URL.'people/');
@@ -21,9 +20,9 @@ define('BUILDER_TEMP_DIR','/tmp/foafbuilder_temporary_file');
 
 define('PRIVATE_EP','http://luma:8090/sparql/');
 define('PUBLIC_EP', 'http://luma:8083/sparql/');
-*/
-// DEV Settings
-define('BUILDER_URL','http://mischa-foafeditor.qdos.com/builder/');
+
+/* DEV Settings
+define('BUILDER_URL','http://mischa-foafeditor.qdos.com/');
 define('IMAGE_URL',BUILDER_URL.'images/');
 define('PUBLIC_URL',BUILDER_URL.'people/');
 define('PRIVATE_URL','http://private-dev.qdos.com/oauth/');
@@ -34,6 +33,6 @@ define('BUILDER_TEMP_DIR','/tmp/mischafoafeditor_temporary_file');
 
 define('PRIVATE_EP','http://luma:9000/sparql/');
 define('PUBLIC_EP', 'http://luma:9001/sparql/');
-
+*/
 
 ?>
