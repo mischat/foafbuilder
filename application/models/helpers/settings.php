7a7fb638b9722322d36ab3566b52bf502fc1fa81
Mischa Tuffield <mischa.tuffield@garlik.com>
2009-01-05 15:54:28 +0000

    Have added dev settings to the settings file

--- application/models/helpers/settings.php
+++ application/models/helpers/settings.php
@@ -8,7 +8,7 @@ define('QDOS_EXT', 'http://extractor.qdos.com:8081/');
 define('FOAF_PATH', '/usr/local/src/qdos-dev/foaf');
 define('QDOS_ROOT', 'http://qdos.com/');
 
-//TODO MISCHA
+/*Live Settings*/
 define('BUILDER_URL','http://foafbuilder.qdos.com/');
 define('IMAGE_URL',BUILDER_URL.'images/');
 define('PUBLIC_URL',BUILDER_URL.'people/');
@@ -20,4 +20,19 @@ define('BUILDER_TEMP_DIR','/tmp/foafbuilder_temporary_file');
 
 define('PRIVATE_EP','http://luma:8090/sparql/');
 define('PUBLIC_EP', 'http://luma:8083/sparql/');
+
+/* DEV Settings
+define('BUILDER_URL','http://mischa-foafeditor.qdos.com/builder/');
+define('IMAGE_URL',BUILDER_URL.'images/');
+define('PUBLIC_URL',BUILDER_URL.'people/');
+define('PRIVATE_URL','http://private-dev.qdos.com/oauth/');
+define('PRIVATE_DATA_DIR', '/usr/local/data/foaf-dev-mischa/private/oauth');
+define('PUBLIC_DATA_DIR','/usr/local/data/foaf-dev-mischa/public');
+define('IMAGE_DATA_DIR','/usr/local/data/foaf-dev-mischa/images');
+define('BUILDER_TEMP_DIR','/tmp/mischafoafeditor_temporary_file');
+
+define('PRIVATE_EP','http://luma:9000/sparql/');
+define('PUBLIC_EP', 'http://luma:9001/sparql/');
+*/
+
 ?>
