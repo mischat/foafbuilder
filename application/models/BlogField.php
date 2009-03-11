61ba1dc88ff189a5a67d4652a0e7c8ce2665cb8e
Mischa Tuffield <mischa.tuffield@garlik.com>
2009-03-11 22:43:32 +0000

    Have fixed domains, without trailing slashes

--- application/models/BlogField.php
+++ application/models/BlogField.php
@@ -116,6 +116,9 @@ class BlogField extends Field {
                     if (!preg_match('/https{0,1}:\/\//',$thisValue)) {
                         $thisValue = "http://".$thisValue;
                     }
+                    if (!preg_match('/https{0,1}:\/\/.*\/.*/',$thisValue)) {
+                        $thisValue = $thisValue."/";
+                    }
                     if ($this->isHomepageValid($thisValue)) {
                         $new_statement = new Statement($primary_topic_resource,$predicate_resource,new Resource($thisValue));	
                         $foafData->getModel()->addWithoutDuplicates($new_statement);
